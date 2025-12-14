3ff14b71fac65ffa7c20f1f4a4f91625667bfbe1
24331a05h0-python <24331a05h0@mvgrce.edu.in>
2025-12-14 20:01:23 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+rollnumber=24331501
+print("integer value:",rollnumber,"type: ",type(rollnumber))
+grade='s'
+print("character value:",grade,"type: ",type(grade))
+cgpa=8.90
+print("float value:",cgpa,"type: ",type(cgpa))
+name="sravya"
+print("string value:",name,"type: ",type(name))
