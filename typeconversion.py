15313c103af4b9aee12e54e8a3576a6f271e5e0b
24331a05h0-python <24331a05h0@mvgrce.edu.in>
2025-12-14 21:09:30 +0530

    Create typeconversion.py

---
+++ typeconversion.py
@@ -0,0 +1,10 @@
+num=float(input("enter a number: "))
+print("value is: ",num)
+print("type is: ",type(num))
+num1=int(num)
+print("value is: ",num1)
+print("type is: ",type(num1))
+a="50"
+print("type conversion from string to int: ",int(a))
+b="7.89"
+print("type conversion from string to float: ",float(b))
