760678aea66c65e352585a75971f635c76775d1b
24331a05h0-python <24331a05h0@mvgrce.edu.in>
2025-12-14 21:30:20 +0530

    mathmodule.py

---
+++ mathmodule.py
@@ -0,0 +1,9 @@
+import math
+print("value of pi: ",math.pi)
+r=float(input("enter the r value: "))
+print("area of circle is: ",math.pi*r*r)
+print("circumference of a circle: ",2*math.pi*r)
+print("square root is: ",math.sqrt(25))
+print("square root of another number is: ",math.sqrt(525))
+print("cos value: ",math.cos(30))
+print("sin value: ",math.sin(45))
