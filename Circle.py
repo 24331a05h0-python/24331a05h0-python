50a693458f9c02c11664f431a00b690de204b1b2
24331a05h0-python <24331a05h0@mvgrce.edu.in>
2025-12-14 22:44:54 +0530

    Create Circle.py

---
+++ Circle.py
@@ -0,0 +1,7 @@
+import math
+r = float(input("Enter the radius of the circle: "))
+area = math.pi * r * r
+print("Area of the circle is:", area)
+radius = int(input("Enter the radius of the circle: "))
+circlearea = math.pi * radius * radius
+print("Area of the circle is:", circlearea)
